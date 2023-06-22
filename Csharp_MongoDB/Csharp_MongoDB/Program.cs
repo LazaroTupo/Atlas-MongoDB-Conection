1592f3bac5da73f41a82eb78a6143e18903ae17b
Sebastian Castillo <sebastian.castillo2@unmsm.edu.pe>
2023-06-21 22:49:47 -0500

    Add C#

---
+++ Csharp_MongoDB/Csharp_MongoDB/Program.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace Csharp_MongoDB 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            var client = new MongoClient("mongodb://localhost:27017"); //Cambiar el URI al database que desea usar
+            using (IAsyncCursor<BsonDocument> cursor = client.ListDatabases())
+            {
+                Console.WriteLine("Nombres de la base de datos seleccionada: \n");
+                while (cursor.MoveNext())
+                {
+                    foreach (var doc in cursor.Current)
+                    {
+                        Console.WriteLine(doc["name"]);
+                    }
+                }
+            }
+        }
+    }
+}
